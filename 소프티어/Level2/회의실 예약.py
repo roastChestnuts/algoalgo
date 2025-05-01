4289a622f58fa50c034a4cf5842d6a2f5fe14005
AhnByeongChan <like10495@gmail.com>
2025-05-01 21:38:26 +0900

    소프티어 회의실 예약 문제

--- 소프티어/Level2/회의실 예약.py
+++ 소프티어/Level2/회의실 예약.py
@@ -41,48 +41,60 @@
 # 요구하는 자료구조는 Map<String, List>형식으로 예상
 
 import sys
+
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+meeting_rooms = dict()
 
-meeting_room = dict()
-
-#회의실 생성
 for _ in range(N):
     room_name = input().rstrip()
-    meeting_room[room_name] = list(True for i in range(24))
+    meeting_rooms[room_name] = [True for i in range(24)]
 
-#회의실 예약시간 입력 후 사용가능 시간대 세팅
 for i in range(M):
     room_name, start, end = input().split()
     start = int(start)
     end = int(end)
-    for time in range(start, end):
-        meeting_room[room_name][time] = False
 
-sorted_room = sorted(meeting_room.keys())
-for idx, room in enumerate(sorted_room):
-    reserve = meeting_room[room] #정렬된 현재 룸
-    start_time = None
+    for time in range(start, end): #끝 시간은 제외, 9-10시라면 10시부턴 예약이 가능 즉, 9시만 False처리
+        meeting_rooms[room_name][time] = False 
+
+#출력(*오름차순으로 출력)
+
+sorted_rooms = sorted(meeting_rooms.keys())
+
+#이제 출력해야하는데 정렬된 순서대로 출력해야하니 sorted_rooms를 기준으로 for를 돌아야함
+#마지막 출력인 경우 '-----'가 출력되면 안됨, 따라서 인덱스도 필요함
+#시작시간과 종료시간이 한 쌍으로 출력돼야 함 -> 튜플자료형을 이용, 개수도 출력해야하니 리스트에 튜플
+#시작시간을 비워놓고서 순회하면서 예약되지 않은 시간이 있다면 시작시간에 담고, 
+#예약된 시간을 만나면 리스트에 튜플로 값을 담아주기
+
+for idx, key in enumerate(sorted_rooms):
+    start = None
     intervals = []
-    for time in range(9, 18):
-        if reserve[time] == True:
-            if start_time == None:
-                start_time = time
-        else:
-            if start_time != None:
-                intervals.append((start_time, time))
-                start_time = None
-    if start_time != None:
-        intervals.append((start_time, 18))
+    check_room = meeting_rooms[key] #현재 순회할 예약실 정보
 
-    print(f'Room {room}:')
+    for time in range(9, 18):
+        if check_room[time] == True:
+            if start == None:
+                start = time
+        else:
+            #시작 시간이 존재한다면 종료시간과 함께 리스트에 담아주기
+            if start != None:
+                intervals.append((start, time))
+                #시작시간 초기화
+                start = None
+    #시작 시간이 존재한다면 17시까지 종료시간을 만나지 못한것이므로 18시까지 예약가능
+    if start != None:
+        intervals.append((start, 18))
+
+    print(f"Room {key}")
     if len(intervals) == 0:
-        print('Not available')
+        print("Not available")
     else:
-        print(f'{len(intervals)} available')    
+        print(f"{len(intervals)} available:")
         for start, end in intervals:
             print(f"{start:02d}-{end:02d}")
-    if idx != len(sorted_room) - 1 :
-        print("-----")        
-    
+
+    if idx != len(intervals)-1:
+        print("-----")                
